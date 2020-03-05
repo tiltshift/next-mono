6b3cc1d4a2fcb33584fec0d56f8fa933a9016bca
Brandon <brobertstech@gmail.com>
2020-03-05 14:11:41 -0800

    Copy example outright

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,4 +1,23 @@
-import apolloServer from "../../server";
+import { ApolloServer, gql } from "apollo-server-micro";
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+  }
+  type User {
+    name: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users(parent, args, context) {
+      return [{ name: "Nextjs" }];
+    }
+  }
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 export const config = {
   api: {
