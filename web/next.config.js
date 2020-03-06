1fa91eb2d0e3f6715c1fcfcb183f9d0557cfc362
Brandon <brobertstech@gmail.com>
2020-03-06 10:36:59 -0800

    Take out un-needed package.
    Still not working. Testing using yarn dev

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,3 @@
-const withPlugins = require("next-compose-plugins");
-
 const withTM = require("next-transpile-modules")(["@calm/server"]);
 
-module.exports = withPlugins([withTM]);
+module.exports = withTM;
