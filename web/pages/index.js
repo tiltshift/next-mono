ca0140320317e63095d3833c310901585e616291
Brandon <brobertstech@gmail.com>
2020-03-06 13:46:31 -0800

    Take out @nextmono/server import

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,16 +1,17 @@
 import fetch from "isomorphic-unfetch";
 import { BobSaget } from "@nextmono/components";
-import apolloServer from "@nextmono/server";
-console.log("apolloServer", apolloServer);
-const Index = ({ users }) => (
+
+const Index = ({ stars }) => (
   <div>
     TURN DOWN FOR WHAT: <BobSaget />
+    <div>{stars}</div>
   </div>
 );
 
-/*
 Index.getInitialProps = async () => {
+  /*
   try {
+    
     const response = await fetch("http://localhost:3000/api/graphql", {
       method: "POST",
       headers: {
@@ -26,8 +27,11 @@ Index.getInitialProps = async () => {
   } catch (error) {
     console.error("error", error);
   }
-  return { users: [] };
+  */
+
+  const res = await fetch("https://api.github.com/repos/zeit/next.js");
+  const json = await res.json();
+  return { stars: json.stargazers_count };
 };
-*/
 
 export default Index;
